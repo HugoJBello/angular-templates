980458ba5381c1eeeac55e4ccf0b567276d04e60
Hugo José Bello Gutiérrez <hjbg@gmv.es>
2018-01-08 16:44:58 +0100

    bla

--- picam-view-angular2/ng2auth/src/app/app.component.ts
+++ picam-view-angular2/ng2auth/src/app/app.component.ts
@@ -5,27 +5,33 @@ import { AuthService } from './auth/auth.service';
   selector: 'app-root',
   template: `
     <div class="container">
-      <nav class="navbar navbar-default">
-        <div class="navbar-header">
-          <a class="navbar-brand" routerLink="/">{{ title }}</a>
-        </div>
+    <div class="navbar navbar-default">
+    <button type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" class="navbar-toggle">
+      <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span>
+      <span class="icon-bar"></span><span class="icon-bar"></span>
+    </button>
+      <div class="navbar-header">
+        <a class="navbar-brand" routerLink="/">{{ title }}</a>
+      </div>
+      <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-          <li>
-            <a routerLink="/deals" routerLinkActive="active">Home</a>
-          </li>
-          <li>
-            <a routerLink="/special" *ngIf="authService.authenticated" routerLinkActive="active">Camera View</a>
-          </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a *ngIf="!authService.authenticated" (click)="authService.login()">Log In</a>
-          </li>
-          <li>
-            <a (click)="authService.logout()" *ngIf="authService.authenticated">Log Out</a>
-          </li>
-        </ul>
-      </nav>
+        <li>
+          <a routerLink="/deals" routerLinkActive="active">Home</a>
+        </li>
+        <li>
+          <a routerLink="/special" *ngIf="authService.authenticated" routerLinkActive="active">Camera View</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a *ngIf="!authService.authenticated" (click)="authService.login()">Log In</a>
+        </li>
+        <li>
+          <a (click)="authService.logout()" *ngIf="authService.authenticated">Log Out</a>
+        </li>
+      </ul>
+      </div>
+      </div>
       <div class="col-sm-12">
         <router-outlet></router-outlet>
       </div>
