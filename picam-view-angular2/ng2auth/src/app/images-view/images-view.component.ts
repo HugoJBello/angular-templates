38d2cc2d6299b5ee8f604092f24ac4738df6e73f
Hugo José Bello Gutiérrez <hjbg@gmv.es>
2018-01-10 14:14:45 +0100

    added button that now works. Form works

--- picam-view-angular2/ng2auth/src/app/images-view/images-view.component.ts
+++ picam-view-angular2/ng2auth/src/app/images-view/images-view.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ImagesService } from '../images.service';
 import { AuthService } from '../auth/auth.service';
-import { Subscription } from 'rxjs/Subscription';
 import { Image } from '../image';
 
 
@@ -10,35 +8,17 @@ import { Image } from '../image';
   templateUrl: './images-view.component.html',
   styleUrls: ['./images-view.component.css']
 })
-export class ImagesViewComponent implements OnInit, OnDestroy {
-  imagesSub: Subscription;
+export class ImagesViewComponent implements OnInit {
   images: Image[];
-  numberImages:number = 1;
-  date:Date = new Date();
-  error: any;
 
-  constructor(public imagesService: ImagesService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.imagesSub = this.imagesService
-      .getImagesDatePaged(this.formatDate(this.date),this.numberImages)
-      .subscribe(
-        images => this.images = images,
-        err => error => this.error = err
-      );
-  }
-  formatDate(date){
-    return date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' +  ("0" + date.getDate()).slice(-2);
-  }
+   }
+ 
 
   onImagesSearch(images){
-    console.log(images);
-    console.log("'¡'¡¡¡¡¡¡'¡¡'¡¡''¡''¡");
     this.images=images;
-  }
-
-  ngOnDestroy() {
-    this.imagesSub.unsubscribe();
   }
 
 }
--- picam-view-angular2/ng2auth/src/app/query-images/query-images.component.ts
+++ picam-view-angular2/ng2auth/src/app/query-images/query-images.component.ts
@@ -1,30 +1,31 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import {ImageQuery} from './imageQuery';
-import {Image} from '../image';
+import { ImageQuery } from './imageQuery';
+import { Image } from '../image';
 import { ImagesService } from '../images.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs/Subscription';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-query-images',
   templateUrl: './query-images.component.html',
   styleUrls: ['./query-images.component.css']
 })
-export class QueryImagesComponent implements OnInit {
-   imageQuery: ImageQuery = new ImageQuery(10, 1,false,false,new Date());
-   images : Image[];
-   imagesSub: Subscription;
-   error: any;
+export class QueryImagesComponent implements OnInit, OnDestroy {
+  imageQuery: ImageQuery = new ImageQuery(10, 1, false, false, new Date());
+  images: Image[];
+  imagesSub: Subscription;
+  error: any;
 
 
-  @Output() onImagesSearch= new EventEmitter<Image[]>();
+  @Output() onImagesSearch = new EventEmitter<Image[]>();
 
   dateNow = new Date();
   options = [
-    {id: 1, name: "1"},
-    {id: 10, name: "10"},
-    {id: 30, name: "30"},
-    {id: "all", name: "all"}
+    { id: 1, name: "1" },
+    { id: 10, name: "10" },
+    { id: 30, name: "30" },
+    { id: "all", name: "all" }
   ];
   selectedValue = this.options[1];
 
@@ -33,10 +34,10 @@ export class QueryImagesComponent implements OnInit {
 
   public onClickButton(): void {  // event will give you full breif of action
     this.imagesSub = this.imagesService
-      .getImagesDatePaged(this.formatDate(this.imageQuery.date),this.imageQuery.page)
+      .getImagesDatePaged(this.formatDate(this.imageQuery.date), this.imageQuery.page)
       .subscribe(
-        images => this.images = images,
-        err => error => this.error = err,
+      images => this.images = images,
+      err => error => this.error = err,
       // The 3rd callback handles the "complete" event.
       () => this.onImagesSearch.emit(this.images)
       );
@@ -44,18 +45,21 @@ export class QueryImagesComponent implements OnInit {
 
   }
 
-  formatDate(date){
+  formatDate(date) {
     alert(date);
     return date;//date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' +  ("0" + date.getDate()).slice(-2);
   }
 
   ngOnInit() {
     this.imagesSub = this.imagesService
-      .getImagesDatePaged(this.formatDate(new Date()),1)
+      .getImagesDatePaged(this.formatDate(new Date()), 1)
       .subscribe(
-        images => this.images = images,
-        err => error => this.error = err
+      images => this.images = images,
+      err => error => this.error = err
       );
+  }
+  ngOnDestroy() {
+    this.imagesSub.unsubscribe();
   }
 
 }
